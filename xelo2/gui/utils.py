ff68526c844ef8aa335d743d8796d7f2d6d2737b
erdi <e.erdal@umcutrecht.nl>
2025-02-28 15:39:41 +0100

    ASP-107 Instead of disabling I went with changing the color to gray for those without a link between files and channels

--- xelo2/gui/utils.py
+++ xelo2/gui/utils.py
@@ -162,38 +162,64 @@ def _mark_channel_file_visual(recording_files: FilesWidget, channels: [], params
     """Filter on dates 2025 and onwards. will check if the path under files and the based_on_file under channel_group
     have any matches, if it does it will mark both of them visually. It will mark any of the non-matching channel
     entries to disable and be non-interactable."""
+
     # First hit it will find under the dict params with session and data created
     _dict_result = next(
         (item for item in params if item["level"] == "Sessions" and item["parameter"] == "Data Created"), None
     )
+
     _timestamp_data_created = _dict_result["value"].date()  # extract the date
     if _timestamp_data_created.year() >= 2025:
         _ = get_fp_rec_file(recording_files, True)
-        # print(f"_mark_channel_file_visual() = {_}")
 
         _channel_based_on_file = []
-        _channel_id = []
         for channel in channels:
             _channel_based_on_file.append(channel.based_on_file)
-            _channel_id.append(channel.id)
-        # print(f"_channel_based_on_file = {_channel_based_on_file}   _channel_group_id = {_channel_id}")
 
+        # Check if there is any overlap in the rec_filepath and channel.based_on_file filepath
         _hits_for_both_filepaths = set(_) & set(_channel_based_on_file)
-        # print(f"if we have a link this is it = {_hits_for_both_filepaths}")
 
-        # Mark the hits based on the _hits_for_both_filepaths
-        if _hits_for_both_filepaths is not None:
-            # Files widget
-            for row in range(recording_files.rowCount()):
-                if recording_files.item(row, 2).text() in _hits_for_both_filepaths:
-                    recording_files.item(row, 0).setBackground(QColor("green"))
-                    recording_files.item(row, 1).setBackground(QColor("green"))
-                    recording_files.item(row, 2).setBackground(QColor("green"))
+        # Mark the hits/misses based on the _hits_for_both_filepaths
+        _iterate_filewidget_actions(recording_files, _hits_for_both_filepaths, QColor("green"))
+        _iterate_channels_actions(channels, channels_listed, _hits_for_both_filepaths, QColor("green"))
 
-            # Channel widget
-            for idx, channel in enumerate(channels):
-                if channel.based_on_file in _hits_for_both_filepaths:
-                    channels_listed.item(idx).setBackground(QColor("green"))
+
+def _iterate_filewidget_actions(file_widget: FilesWidget, check_list: set, change_color: QColor) -> None:
+    """Generic internal function to take a filewidget class based on QTableWidget and iterate through the items inside
+    the widget with the option to check if some of its values are present in a set as comparison. It will also
+    allow for an action, such as changing the color of the item."""
+    if check_list is not None:
+        for row in range(file_widget.rowCount()):
+            if file_widget.item(row, 2).text() in check_list:
+                file_widget.item(row, 0).setBackground(change_color)
+                file_widget.item(row, 1).setBackground(change_color)
+                file_widget.item(row, 2).setBackground(change_color)
+            else:
+                file_widget.item(row, 0).setBackground(QColor("gray"))
+                file_widget.item(row, 1).setBackground(QColor("gray"))
+                file_widget.item(row, 2).setBackground(QColor("gray"))
+
+    if len(check_list) == 0:  # if empty set
+        for row in range(file_widget.rowCount()):
+            file_widget.item(row, 0).setBackground(QColor("gray"))
+            file_widget.item(row, 1).setBackground(QColor("gray"))
+            file_widget.item(row, 2).setBackground(QColor("gray"))
+
+
+def _iterate_channels_actions(channels: [], channels_listed: Any, check_list: set, change_color: QColor) -> None:
+    """Generic internal function to take a list of channels and iterate through the items inside the widget with the
+     option to check if some of its values are present in a set as comparison. It will also allow for an action, such as
+     changing the color of the item."""
+    if check_list is not None:
+        for idx, channel in enumerate(channels):
+            if channel.based_on_file in check_list:
+                channels_listed.item(idx).setBackground(change_color)
+            else:
+                channels_listed.item(idx).setBackground(QColor("gray"))
+
+    if len(check_list) == 0:  # if empty set
+        for idx, channel in enumerate(channels):
+            channels_listed.item(idx).setBackground(QColor("gray"))
 
 
 def get_fp_rec_file(t_files: FilesWidget, return_list: bool = False) -> Union[str, list]:
