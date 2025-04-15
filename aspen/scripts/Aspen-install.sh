b973ce1e93374f4a9e9f31f2da925f1e274617df
erdi <e.erdal@umcutrecht.nl>
2025-04-15 15:23:34 +0200

    ASP-104 inclusion of the bash file.

---
+++ aspen/scripts/Aspen-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+apt install python3-pyqt5  python3-pyqt5.qtsql  python3-numpy  python3-pandas
+
+
+# Pull the code
+mkdir -p /usr/local/aspen
+cd /usr/local/aspen
+if [ -d ASPEN ]; then
+	cd ASPEN
+	git pull
+else
+	git clone https://github.com/UMCU-RIBS/ASPEN.git
+	cd ASPEN
+fi
+git checkout master
