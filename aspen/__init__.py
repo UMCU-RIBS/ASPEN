a1c401f58905014675ac21f24fcee4ba08d94ba5
erdi <e.erdal@umcutrecht.nl>
2025-11-03 11:28:24 +0100

    ASP-190 Slight change of changelog formatting

--- aspen/__init__.py
+++ aspen/__init__.py
@@ -2,10 +2,10 @@ __version__ = "1.0.8"
 
 
 __changelog__ = (
-    "v1.0.8: \n \n "
-    "UI change to display parameter names without cutting off due to strecht properties \n "
+    "\n v1.0.8: \n"
+    "UI change to display parameter names without cutting off due to strecht properties \n \n"
     
-    "v1.0.7: \n \n "
+    "v1.0.7: \n"
     "The following fields are hidden for BCI sessions \n "
     "ASCA Score, Battery Level, Mood, Motivation, Tiredness Pre, Tiredness Post \n \n"
     "The following fields are hidden for all sessions \n"
